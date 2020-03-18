0a6ab78617846271867ef91ad567e1629c5e4bf4
Dan Mindru <mindrudan@gmail.com>
2020-03-18 23:44:45 +0100

    Move utils to own modules

---
+++ src/utils/makeCommand.js
@@ -0,0 +1,24 @@
+import { isDebugOn } from "./debug";
+
+/**
+ * Makes a command with that can accept args
+ * @param { string } command
+ */
+export const makeCommand = (command) => (args): Promise<any> => {
+  // Send a command
+  if (isDebugOn()) {
+    console.log('commands args', args);
+  }
+
+  return new Promise((resolve, reject) => {
+    const cmd = `${command} ${args._.join(' ')} ${Object.keys(args)
+      .filter((key) => key !== '_')
+      .map((key) => `${key}=${args[key]}`)}`;
+
+    if (isDebugOn()) {
+      console.log('Command to send', cmd);
+    }
+
+    resolve(cmd);
+  });
+};
