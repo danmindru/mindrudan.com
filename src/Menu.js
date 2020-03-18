b169a3e0bd111667ec734d9790461bef1c344b28
Dan Mindru <mindrudan@gmail.com>
2020-03-18 23:45:52 +0100

    Update menu to include more commands

--- src/Menu.js
+++ src/Menu.js
@@ -1,32 +1,65 @@
 import React from 'react';
 import { css } from 'emotion';
 
-const menuStyle = css`
-  --menu-width: 280px;
+import { ReactComponent as GithubIcon } from './icons/github-icon.svg';
+import { ReactComponent as StackoverflowIcon } from './icons/stackoverflow-icon.svg';
+import { ReactComponent as TwitterIcon } from './icons/twitter-icon.svg';
+import { ReactComponent as LinkedinIcon } from './icons/linkedin-icon.svg';
+import { COMMAND_NAMES } from './commands/command-constants';
+import { wait } from './utils/wait';
 
-  position: absolute;
+const menuStyle = css`
+  --menu-width: 300px;
+
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 32px);
+  padding: 32px;
   transform: translate3d(var(--menu-width), 0, 0);
+  position: absolute;
   z-index: 3;
   top: 0;
   right: 0;
-  height: 100vh;
-  overflow: hidden;
+  overflow: auto;
   transition: transform 0.8s;
-  background-color: rgba(251, 115, 121, 1);
+  background-color: rgba(251, 115, 121, 0.7);
+  backdrop-filter: blur(3px);
   color: white;
-  padding: 32px;
   box-shadow: 5px 5px 10px 10px rgba(0, 0, 0, 0.2);
+`;
 
-  ul {
-    height: 100%;
-    overflow: auto;
-    list-style-type: none;
+const menuListStyle = css`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  overflow: hidden;
+  list-style-type: none;
+  margin: 0;
+  padding: 32px 0;
+
+  li {
     margin: 0;
-    padding: 32px 0;
+    padding: 8px 16px;
 
-    li {
-      margin: 0;
-      padding: 8px 16px;
+    svg {
+      margin-right: 8px;
+    }
+
+    a {
+      display: flex;
+      align-items: center;
+    }
+  }
+`;
+
+const menuListSocialStyle = css`
+  margin-top: auto;
+
+  li {
+    &:first-child {
+      margin-top: 16px;
+      padding-top: 16px;
+      border-top: 1px solid rgba(255, 255, 255, 0.2);
     }
   }
 `;
@@ -99,19 +132,48 @@ const menuToggleStyleOpen = css`
 export const Menu = (props) => {
   const { open, setOpen, runCommand } = props;
 
-  const aboutPressed = () => {
+  const aboutPressed = async () => {
     setOpen(false);
-    runCommand('whoami');
+
+    runCommand(COMMAND_NAMES.CLEAR);
+    await wait(100);
+    runCommand(COMMAND_NAMES.PHOTO);
+    await wait(1000)
+    runCommand(COMMAND_NAMES.WHOAMI);
+  };
+
+  const workPressed = async () => {
+    setOpen(false);
+
+    runCommand(COMMAND_NAMES.CLEAR);
+    await wait(100);
+    runCommand(COMMAND_NAMES.WORK);
+  };
+
+  const contactPressed = async () => {
+    setOpen(false);
+
+    runCommand(COMMAND_NAMES.CLEAR);
+    await wait(100);
+    runCommand(COMMAND_NAMES.CONTACT);
+  };
+
+  const screensaverPressed = async () => {
+    setOpen(false);
+    runCommand(COMMAND_NAMES.SCREENSAVER)
   };
 
   return (
     <>
-      <button className={[menuToggleStyle, open ? menuToggleStyleOpen : ''].join(' ')} onClick={() => setOpen(!open)}>
+      <button
+        className={[menuToggleStyle, open ? menuToggleStyleOpen : ''].join(' ')}
+        onClick={() => setOpen(!open)}
+      >
         {open ? 'Open' : 'Close'}
       </button>
 
       <nav className={[menuStyle, open ? menuOpenStyle : ' '].join(' ')}>
-        <ul>
+        <ul className={menuListStyle}>
           <li>
             <button className={menuButtonStyle} onClick={() => setOpen(!open)}>
               Terminal
@@ -123,19 +185,61 @@ export const Menu = (props) => {
             </button>
           </li>
           <li>
-            <button className={menuButtonStyle}>Projects</button>
+            <button onClick={contactPressed} className={menuButtonStyle}>
+              Contact
+            </button>
           </li>
           <li>
-            <button className={menuButtonStyle}>Screensaver</button>
+            <button onClick={workPressed} className={menuButtonStyle}>
+              Work
+            </button>
           </li>
           <li>
-            <a href="https://github.com/danmindru" target="_blank" rel="noopener noreferrer" className={menuButtonStyle}>
-              Github @danmindru
+            <button onClick={screensaverPressed} className={menuButtonStyle}>
+              Screensaver
+            </button>
+          </li>
+        </ul>
+
+        <ul className={[menuListStyle, menuListSocialStyle].join(' ')}>
+          <li>
+            <a
+              href="https://github.com/danmindru"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={menuButtonStyle}
+            >
+              <GithubIcon /> @danmindru
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/d4m1n" target="_blank" rel="noopener noreferrer" className={menuButtonStyle}>
-              Twitter @d4m1n
+            <a
+              href="https://stackoverflow.com/users/3263450/dan-mindru"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={menuButtonStyle}
+            >
+              <StackoverflowIcon /> dan-mindru
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/d4m1n"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={menuButtonStyle}
+            >
+              <TwitterIcon /> @d4m1n
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://dk.linkedin.com/in/danmindru"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={menuButtonStyle}
+            >
+              <LinkedinIcon /> Dan Mindru
             </a>
           </li>
         </ul>
