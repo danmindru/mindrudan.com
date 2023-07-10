1b8359aa00db9d89ff04a1aa560622592c9d4c23
Dan Mindru <mindrudan@gmail.com>
2023-07-10 11:02:16 +0200

    Enabled va

--- packages/blog/data/siteMetadata.js
+++ packages/blog/data/siteMetadata.js
@@ -21,16 +21,16 @@ const siteMetadata = {
   youtube: '',
   linkedin: 'https://www.linkedin.com/in/danmindru',
   locale: 'en-US',
-  analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    posthogProjectApiKey: '', // e.g. AhnJK8392ndPOav87as450xd
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
-  },
+  // analytics: {
+  //   // If you want to use an analytics provider you have to add it to the
+  //   // content security policy in the `next.config.js` file.
+  //   // supports plausible, simpleAnalytics, umami or googleAnalytics
+  //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+  //   simpleAnalytics: false, // true or false
+  //   umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+  //   posthogProjectApiKey: '', // e.g. AhnJK8392ndPOav87as450xd
+  //   googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  // },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
