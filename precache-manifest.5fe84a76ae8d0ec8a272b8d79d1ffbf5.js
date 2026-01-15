75fc0e8ce85413609440de79c2aa948b8ed5a3f7
Dan Mindru <mindrudan@gmail.com>
2026-01-15 22:20:41 +0100

    Updates

---
+++ precache-manifest.5fe84a76ae8d0ec8a272b8d79d1ffbf5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f31ce5baeb43f44c9d599a78fa2799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e665dc0ed2399cb6c0b9",
+    "url": "/static/css/2.1913025f.chunk.css"
+  },
+  {
+    "revision": "8cca63b05bc7d408bbf6",
+    "url": "/static/css/main.09539279.chunk.css"
+  },
+  {
+    "revision": "e665dc0ed2399cb6c0b9",
+    "url": "/static/js/2.28019ef8.chunk.js"
+  },
+  {
+    "revision": "498560264d04be2e91549014fd9616df",
+    "url": "/static/js/2.28019ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6b9bcf0ba97fea2388",
+    "url": "/static/js/3.91784c33.chunk.js"
+  },
+  {
+    "revision": "ec5a5bb5f91cee01f71a",
+    "url": "/static/js/4.65a6c790.chunk.js"
+  },
+  {
+    "revision": "568e752920ba8961938c",
+    "url": "/static/js/5.5b124a08.chunk.js"
+  },
+  {
+    "revision": "8f1bbcd877da94f5a86c",
+    "url": "/static/js/6.38747a1a.chunk.js"
+  },
+  {
+    "revision": "21335c207c29fe85b182",
+    "url": "/static/js/7.bbaea1d8.chunk.js"
+  },
+  {
+    "revision": "3044b33721e767c98ae4",
+    "url": "/static/js/8.0d10dfba.chunk.js"
+  },
+  {
+    "revision": "67676a097ed27ea75796",
+    "url": "/static/js/9.cb4f4035.chunk.js"
+  },
+  {
+    "revision": "8cca63b05bc7d408bbf6",
+    "url": "/static/js/main.05afd7dc.chunk.js"
+  },
+  {
+    "revision": "6a291c259c82d79a73d2",
+    "url": "/static/js/runtime-main.f0cf373d.js"
+  },
+  {
+    "revision": "101aae49a1daedf4f10472a135c399d1",
+    "url": "/static/media/github-icon.101aae49.svg"
+  },
+  {
+    "revision": "799c578ee40f149bf44d7d271d3b3470",
+    "url": "/static/media/linkedin-icon.799c578e.svg"
+  },
+  {
+    "revision": "943dafa23fd5964dd709719596be0299",
+    "url": "/static/media/stackoverflow-icon.943dafa2.svg"
+  },
+  {
+    "revision": "1459273fcc9ef595c74933a6aabc15fe",
+    "url": "/static/media/twitter-icon.1459273f.svg"
+  },
+  {
+    "revision": "3d448a6660ade6a25c6f23ad3f2496b7",
+    "url": "/static/media/youtube-icon.3d448a66.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f698dcd98712af7d0fefa838be583ac.js"
+  "/precache-manifest.5fe84a76ae8d0ec8a272b8d79d1ffbf5.js"
 );
 
 self.addEventListener('message', (event) => {
